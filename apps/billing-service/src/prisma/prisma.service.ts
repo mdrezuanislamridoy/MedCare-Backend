5cb3c406f3e07a41f44d560231ecde00030bac77
mdrezuanislamridoy <mdrezuanislamridoy@gmail.com>
2026-08-19 15:56:17 +0600

    implement Prisma service injection and define database schemas for clinic, notification, audit, and billing services

---
+++ apps/billing-service/src/prisma/prisma.service.ts
@@ -0,0 +1,13 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '../../../../generated/prisma/client';
+
+@Injectable()
+export class BillingPrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
